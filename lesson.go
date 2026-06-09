8388d343474d3fb224fdf90a8bece2ad76bd9d63
Hanai <yuuukariforev626111@gmail.com>
2026-06-09 23:03:46 +0900

    標準出力の記載の仕方

--- lesson.go
+++ lesson.go
@@ -1,1 +1,17 @@
-// This is Go Main Func 
+// This is Go Main Func
+package main // これは必ず記載する
+
+import "fmt"
+
+func init() {
+	fmt.Println("Init!")
+}
+
+func bazz() {
+	fmt.Println("bazz")
+}
+
+func main() {
+	bazz()
+	fmt.Println("Hello World") // 標準出力には fmt のimport が必要
+}
