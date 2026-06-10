700b90ff08ae13ed2a1cfe8b0eac18592511bb64
Hanai <yuuukariforev626111@gmail.com>
2026-06-10 23:09:32 +0900

    変数宣言の仕方について記載(途中)

--- lesson.go
+++ lesson.go
@@ -1,14 +1,25 @@
 // This is Go Main Func
 package main // これは必ず記載する
 
-import (
-	"fmt"
-	"os/user"
-	"time"
-)
+import "fmt"
 
-// import に関しての講義
+// 変数宣言 に関しての講義
 func main() {
-	fmt.Println("Hello World", time.Now()) // 標準出力には fmt のimport が必要
-	fmt.Println(user.Current())
+	var i int = 20
+	var t bool = true
+	var f bool = false
+	var s string = "Test"
+	var fl float64 = 1.01
+
+	// まとめて宣言
+	var (
+		num       int     = 10
+		is_ok     bool    = true
+		is_not_ok bool    = false
+		str_test  string  = "This is Test"
+		fl_num    float64 = 1.02
+	)
+
+	fmt.Println("標準的な記載の仕方", i, t, f, s, fl)
+	fmt.Println("まとめて宣言をした記載の仕方", num, is_ok, is_not_ok, str_test, fl_num)
 }
