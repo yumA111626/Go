e579b63afa3284c7b110c6a37462d5e0c8809f83
Hanai <yuuukariforev626111@gmail.com>
2026-06-11 22:25:24 +0900

    変数宣言について記載

--- lesson.go
+++ lesson.go
@@ -3,20 +3,27 @@ package main // これは必ず記載する
 
 import "fmt"
 
-// 変数宣言 に関しての講義
-func main() {
-	var i int = 20
-	var t bool = true
-	var f bool = false
-	var s string = "Test"
-	var fl float64 = 1.01
+// 短変数宣言は関数内でしか実行できない var の通常宣言は可能
+var (
+	i  int     = 20
+	t  bool    = true
+	f  bool    = false
+	s  string  = "Test"
+	fl float64 = 1.01
+)
 
-	// 動的型付け宣言
+func foo() {
 	xi := 1
 	xs := "test"
 	xf64 := 1.01
 	xt, xf := true, false
 
-	fmt.Println("標準的な記載の仕方", i, t, f, s, fl)
 	fmt.Println(xi, xs, xf64, xt, xf)
+	fmt.Printf("%T\n", xf64) // 型について出力させる
+}
+
+// 変数宣言 に関しての講義
+func main() {
+	foo() // 関数内では短変数宣言は可能
+	// fmt.Println("標準的な記載の仕方", i, t, f, s, fl)
 }
