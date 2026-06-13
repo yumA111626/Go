99c79f4676c691850f82b57f39c7246cceba0df0
Hanai <yuuukariforev626111@gmail.com>
2026-06-13 12:42:35 +0900

    数値型について記載

--- lesson.go
+++ lesson.go
@@ -1,21 +1,15 @@
 // This is Go Main Func
 package main // これは必ず記載する
 
-import "fmt"
-
-const Pi = 3.14
-
-const (
-	USER_NAME = "user_name"
-	PASS_WORD = "Pass_word"
-)
-
-const Int_Max = 9223372036854775807 + 1
-
-// var max int = 9223372036854775807 + 1 // 変数の場合、コンパイラに Intと判断され、コンパイルエラーになる
-
-// 変数宣言 に関しての講義
+import "fmt" // 数値型 に関しての講義
 func main() {
-	fmt.Println(Pi, USER_NAME, PASS_WORD)
-	fmt.Println(Int_Max - 1) // const の場合はコンパイル時には型の判断がされない
+	// Go では長い物に合わせて記載する
+	var (
+		u8   uint8   = 221
+		f32  float32 = 1.01
+		int2 int     = 344
+	)
+	fmt.Printf("type=%T , Value=%v\n", u8, u8)
+	fmt.Printf("type=%T , Value=%v\n", f32, f32)
+	fmt.Printf("type=%T , Value=%v\n", int2, int2)
 }
