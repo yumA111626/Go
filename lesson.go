6f3ceadb3ed0e3da5182cc2829c25530f6cf135d
Hanai <yuuukariforev626111@gmail.com>
2026-06-18 23:15:15 +0900

    Section 3-13 型変換について記載

--- lesson.go
+++ lesson.go
@@ -1,19 +1,24 @@
 // This is Go Main Func
 package main // これは必ず記載する
 
-import "fmt" // 論理値型 に関しての講義
+import (
+	"fmt" // 型変換 に関しての講義
+	"strconv"
+)
+
 func main() {
-	// var t ,f bool = true , false
-	t, f := true, false
-	fmt.Printf("%T %v\n", t, t)
-	fmt.Printf("%T %v\n", f, f)
+	var x int = 1
+	xx := float64(x)
 
-	// 論理演算子
-	fmt.Println(true && true)
-	fmt.Println(true && false)
-	fmt.Println(false && false)
+	var y float32 = 1.2
+	yy := int(y)
 
-	fmt.Println(true || true)
-	fmt.Println(true || false)
-	fmt.Println(false || false)
+	fmt.Printf("%T , %v , %f\n", xx, xx, xx)
+	fmt.Printf("%T , %v , %d\n", yy, yy, yy)
+
+	// 文字列型の数値を int 型に変換したい(strconv を使用することによって変換可能)
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+
+	fmt.Printf("%T , %v , %d\n", i, i, i)
 }
