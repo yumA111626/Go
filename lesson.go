aafdc790ea3803190fa72baab3fbfc83267bd01b
Hanai <yuuukariforev626111@gmail.com>
2026-06-27 10:44:49 +0900

    Section 3-17 map について記載

--- lesson.go
+++ lesson.go
@@ -5,32 +5,33 @@ import (
 	"fmt" // 型変換 に関しての講義
 )
 
-// makeに関しての講義
+// mapに関しての講義
 func main() {
-	n := make([]int, 3, 5) //make でスライスの作成
-	fmt.Printf("len=%d cap=%d val=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0) // スライスに追加する
-	fmt.Printf("len=%d cap=%d val=%v\n", len(n), cap(n), n)
-	b := make([]int, 0) // 空のスライスを作成する
-	var c []int         // 空のスライスを作成する (メモリを確保しない)
-	fmt.Printf("len=%d cap=%d val=%v\n", len(b), cap(b), b)
-	fmt.Printf("len=%d cap=%d val=%v\n", len(c), cap(c), c)
+	// map とはpython でいう所の辞書のようなもの
+	m := map[string]int{"apple": 200, "banana": 100}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["melon"] = 250 // map に追加をする
+	fmt.Println(m)
+	fmt.Println(m["melon"])
 
-	// 演習
-	fmt.Println("make でスライスのサイズを明示")
-	c = make([]int, 5)
-	for i := 0; i < 5; i++ {
-		c = append(c, i)
-		fmt.Println(c)
-	}
-	fmt.Println(c)
-	fmt.Println("make でスライスのサイズを明示しない")
+	// 存在しないものを出力する
+	fmt.Println(m["nothing"])
 
-	var d int[]
-    d = make([]int, 0, 5)
-	for i := 0; i < 5; i++ {
-		d = append(d, i)
-		fmt.Println(d)
-	}
-	fmt.Println(d)
+	// 存在しているかの判定
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	// メモリ上にからのmapを作成してそこのmapに追加していく
+	m2 := make(map[string]int) // map を作成
+	m2["PC"] = 3000
+	fmt.Println(m2)
+
+	// var での宣言時の注意点
+	var m3 map[string]int
+	m3["PC"] = 5000
+	fmt.Println(m3)
 }
