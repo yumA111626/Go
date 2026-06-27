84ed7dac33e04456854d1df39de84e233447fc0e
Hanai <yuuukariforev626111@gmail.com>
2026-06-27 23:11:23 +0900

    Section 3-19 関数について - 関数内で関数について記載

--- lesson.go
+++ lesson.go
@@ -20,7 +20,7 @@ func add_2(x, y int) (int, int) {
 // 返り値で変数宣言をする
 func cal(price, item int) (result int) {
 	result = price * item
-	return result
+	return result // 変数で返り値を宣言しているため return のみでも良い
 }
 
 // 関数に関しての講義
@@ -32,4 +32,18 @@ func main() {
 
 	r4 := cal(1000, 2)
 	fmt.Println(r4)
+
+	// 関数内で関数の宣言
+	f := func() {
+		fmt.Println("Inner func")
+	}
+	f()
+
+	func() {
+		fmt.Println("Inner func2")
+	}()
+
+	func(x int) {
+		fmt.Println("Inner func2", x)
+	}(1)
 }
