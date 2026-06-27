d6f70f0c44a6f0acd9891da6c01176234549d075
Hanai <yuuukariforev626111@gmail.com>
2026-06-27 22:44:37 +0900

    Section 3-19 関数について - add 関数の追加

--- lesson.go
+++ lesson.go
@@ -5,12 +5,11 @@ import (
 	"fmt" // 型変換 に関しての講義
 )
 
-// byteに関しての講義
-func main() {
-	by := []byte{72, 73} // ASCII Code
-	fmt.Println(by)
+func add() {
+	fmt.Println("add function")
+}
 
-	by_str := []byte("HI")
-	fmt.Println(by_str)
-	fmt.Println(string(by_str))
+// 関数に関しての講義
+func main() {
+	add()
 }
