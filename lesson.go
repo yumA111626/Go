7b264d82feab2953a29057dd4e947cd8291c3288
Hanai <yuuukariforev626111@gmail.com>
2026-06-27 22:26:01 +0900

    Section 3-18 Byte型 に関して記載

--- lesson.go
+++ lesson.go
@@ -5,33 +5,12 @@ import (
 	"fmt" // 型変換 に関しての講義
 )
 
-// mapに関しての講義
+// byteに関しての講義
 func main() {
-	// map とはpython でいう所の辞書のようなもの
-	m := map[string]int{"apple": 200, "banana": 100}
-	fmt.Println(m)
-	fmt.Println(m["apple"])
-	m["melon"] = 250 // map に追加をする
-	fmt.Println(m)
-	fmt.Println(m["melon"])
+	by := []byte{72, 73} // ASCII Code
+	fmt.Println(by)
 
-	// 存在しないものを出力する
-	fmt.Println(m["nothing"])
-
-	// 存在しているかの判定
-	v, ok := m["apple"]
-	fmt.Println(v, ok)
-
-	v2, ok2 := m["nothing"]
-	fmt.Println(v2, ok2)
-
-	// メモリ上にからのmapを作成してそこのmapに追加していく
-	m2 := make(map[string]int) // map を作成
-	m2["PC"] = 3000
-	fmt.Println(m2)
-
-	// var での宣言時の注意点
-	var m3 map[string]int
-	m3["PC"] = 5000
-	fmt.Println(m3)
+	by_str := []byte("HI")
+	fmt.Println(by_str)
+	fmt.Println(string(by_str))
 }
