6555f31acea92520d00e5246d20ea9352fa5e589
Hanai <yuuukariforev626111@gmail.com>
2026-06-27 23:00:20 +0900

    Section 3-19 関数について - 返り値が複数ある関数・返り値で変数宣言する関数について記載

--- lesson.go
+++ lesson.go
@@ -1,15 +1,35 @@
 // This is Go Main Func
 package main // これは必ず記載する
 
-import (
-	"fmt" // 型変換 に関しての講義
-)
+import "fmt" // 型変換 に関しての講義
 
-func add(x int, y int) {
-	fmt.Println(x + y)
+/*
+	同じ型の場合は、引数に関しても省略して記載することは可能
+
+func add(x,y int) int{}
+*/
+func add(x int, y int) int {
+	return x + y
+}
+
+// 返り値が複数存在する
+func add_2(x, y int) (int, int) {
+	return x + y, x - y
+}
+
+// 返り値で変数宣言をする
+func cal(price, item int) (result int) {
+	result = price * item
+	return result
 }
 
 // 関数に関しての講義
 func main() {
-	add(10, 20)
+	r := add(10, 20)
+	r1, r2 := add_2(20, 10) // 返り値が複数存在する
+	fmt.Println(r)
+	fmt.Println(r1, r2)
+
+	r4 := cal(1000, 2)
+	fmt.Println(r4)
 }
