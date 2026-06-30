8c6c0dab6821e603220ea331ad2bdd4a688f2808
Hanai <yuuukariforev626111@gmail.com>
2026-06-30 22:42:04 +0900

    Section 3-20 クロージャーを記載 インナー関数の中の引数を渡し方について記載

--- lesson.go
+++ lesson.go
@@ -12,11 +12,17 @@ func incrementGenerator() func() int {
 	}
 }
 
+// 円の面積を算出する関数(クロージャー)を記載
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return pi * radius * radius
+	}
+}
+
 // クロージャーに関しての講義
 func main() {
-	counter := incrementGenerator()
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
+	c1 := circleArea(3.14)
+	fmt.Println(c1(3))
+	c2 := circleArea(3)
+	fmt.Println(c2(3))
 }
