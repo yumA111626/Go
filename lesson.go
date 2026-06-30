0af90ed703cb1968cbaa55333b54d5a9e4cf78b9
Hanai <yuuukariforev626111@gmail.com>
2026-06-30 22:34:02 +0900

    Section 3-20 クロージャーを記載 関数から関数を返り値で返す処理について記載

--- lesson.go
+++ lesson.go
@@ -3,35 +3,20 @@ package main // これは必ず記載する
 
 import "fmt" // 型変換 に関しての講義
 
-/*
-	同じ型の場合は、引数に関しても省略して記載することは可能
-
-func add(x,y int) int{}
-*/
-func add(x int, y int) int {
-	return x + y
-}
-
-// 返り値が複数存在する
-func add_2(x, y int) (int, int) {
-	return x + y, x - y
-}
-
-// 返り値で変数宣言をする
-func cal(price, item int) (result int) {
-	result = price * item
-	return result // 変数で返り値を宣言しているため return のみでも良い
+func incrementGenerator() func() int {
+	x := 0
+	// インナー関数を記載
+	return func() int {
+		x++
+		return x
+	}
 }
 
 // クロージャーに関しての講義
 func main() {
-	x := 0
-	// インナー関数を記載
-	increment := func() int {
-		x++
-		return x
-	}
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
+	counter := incrementGenerator()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
 }
