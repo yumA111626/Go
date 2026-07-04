726c26093a64d7907d27f6333b40ba5c525c7963
Hanai <yuuukariforev626111@gmail.com>
2026-07-04 17:07:32 +0900

    if 文に関して記載

--- section_4_statement/lesson_24.go
+++ section_4_statement/lesson_24.go
@@ -7,6 +7,15 @@ import "fmt"
 if 文に関しての講義
 ==============
 */
+
+func by2(num int) string {
+	if num%2 == 0 {
+		return "OK"
+	} else {
+		return "NG"
+	}
+}
+
 func main() {
 	x := 9
 	if x%2 == 0 {
@@ -15,5 +24,20 @@ func main() {
 		fmt.Println("By 3")
 	} else {
 		fmt.Println("else")
+	}
+
+	//偶数かを判定する
+	jug := by2(3)
+	if jug == "OK" {
+		fmt.Println("Great")
+	} else {
+		fmt.Println("Oh...")
+	}
+
+	// if ステートメントの中でしか使用しない変数の場合の書き方
+	if result := by2(10); result == "OK" {
+		fmt.Println("Great OK")
+	} else {
+		fmt.Println("Oh.......")
 	}
 }
