3b84e5e998da669790a45ef673d198260937799c
Hanai <yuuukariforev626111@gmail.com>
2026-07-04 15:34:41 +0900

    Section 3-21可変長引数に関して -可変長引数をもつ関数を設定 -

--- lesson.go
+++ lesson.go
@@ -3,26 +3,19 @@ package main // これは必ず記載する
 
 import "fmt" // 型変換 に関しての講義
 
-func incrementGenerator() func() int {
-	x := 0
-	// インナー関数を記載
-	return func() int {
-		x++
-		return x
+/*
+可変長の引数を渡せる関数を作成する場合は、...をつける
+*/
+func foo(params ...int) {
+	fmt.Println(len(params), params)
+	// ループで引数の中身を取り出す
+	for _, param := range params {
+		fmt.Println(param)
 	}
 }
 
-// 円の面積を算出する関数(クロージャー)を記載
-func circleArea(pi float64) func(radius float64) float64 {
-	return func(radius float64) float64 {
-		return pi * radius * radius
-	}
-}
-
-// クロージャーに関しての講義
+// 可変長引数に関しての講義
 func main() {
-	c1 := circleArea(3.14)
-	fmt.Println(c1(3))
-	c2 := circleArea(3)
-	fmt.Println(c2(3))
+	foo(10, 20)
+	foo(10, 20, 30)
 }
