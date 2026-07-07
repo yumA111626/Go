a826305dbd29f8725cf92a9a8f5bdcab6350087c
Hanai <yuuukariforev626111@gmail.com>
2026-07-07 22:34:14 +0900

    break に関して記載

--- section_4_statement/lesson_25.go
+++ section_4_statement/lesson_25.go
@@ -7,7 +7,12 @@ func main() {
 	for i := 0; i < 10; i++ {
 		if i == 3 {
 			fmt.Println("Continue")
-			continue
+			continue // 次のfor にいく
+		}
+
+		if i > 5 {
+			fmt.Println("Break")
+			break
 		}
 		fmt.Println(i)
 	}
