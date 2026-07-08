bdfbf68fde5ce276f816f0e38537ef0d1958fe73
Hanai <yuuukariforev626111@gmail.com>
2026-07-08 22:28:47 +0900

    switch 関数の省略した記載の仕方について記載

--- section_4_statement/lesson_27/lesson_.go
+++ section_4_statement/lesson_27/lesson_.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
+func getOsName() string {
+	return "mac"
+}
+
 // switch に関して記載
 func main() {
 	// switch 文の記載の仕方
 	os := "mac"
 	switch os {
+	case "mac":
+		fmt.Println("Mac!")
+	case "Windows":
+		fmt.Println("Windows!")
+	default: // 条件で一致しない場合
+		fmt.Println("default")
+	}
+
+	// 関数から値を取得
+	os2 := getOsName()
+	switch os2 {
+	case "mac":
+		fmt.Println("Mac!")
+	case "Windows":
+		fmt.Println("Windows!")
+	default: // 条件で一致しない場合
+		fmt.Println("default")
+	}
+
+	// 特殊な記載の仕方
+	switch os3 := getOsName(); os3 {
 	case "mac":
 		fmt.Println("Mac!")
 	case "Windows":
