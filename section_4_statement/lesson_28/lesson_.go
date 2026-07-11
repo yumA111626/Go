80141d05225871c1d09807da9395ed127580cb68
Hanai <yuuukariforev626111@gmail.com>
2026-07-12 08:15:40 +0900

    スタッキングdefer に関して記載

--- section_4_statement/lesson_28/lesson_.go
+++ section_4_statement/lesson_28/lesson_.go
@@ -13,9 +13,23 @@ func foo() {
 	fmt.Println("Hello foo")
 }
 
-func main() {
-	foo()
+// stacking_defer に関して記載
+// したから順に実行されていく
+func stacking_defer() {
+	fmt.Println("Run")
 
-	defer fmt.Println("world")
-	fmt.Println("Hello")
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+
+	fmt.Println("Success")
+
+}
+
+func main() {
+	// foo()
+
+	// defer fmt.Println("world")
+	// fmt.Println("Hello")
+	stacking_defer()
 }
