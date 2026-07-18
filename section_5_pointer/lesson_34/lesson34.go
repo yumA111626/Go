0f2766e434fd807255618256af5fdb27439192ea
Hanai <yuuukariforev626111@gmail.com>
2026-07-18 15:31:08 +0900

    デリファレンスに関して記載

--- section_5_pointer/lesson_34/lesson34.go
+++ section_5_pointer/lesson_34/lesson34.go
@@ -2,13 +2,29 @@ package main
 
 import "fmt"
 
+// 普通の関数
+func one(x int) {
+	x = 1
+}
+
+// 普通の関数
+func one_pointer(x *int) {
+	*x = 1
+}
+
 // ポインタについての講義
 func main() {
 	var n int = 100
+	one(n)
 	fmt.Println(n)
-	fmt.Println(&n)
+	one_pointer(&n)
+	fmt.Println(n)
+	/*
+		fmt.Println(n)
+		fmt.Println(&n)
 
-	var p *int = &n
-	fmt.Println(p)
-	fmt.Println(*p)
+		var p *int = &n
+		fmt.Println(p)
+		fmt.Println(*p)
+	*/
 }
