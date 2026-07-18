b4d90ef6bb4fc4b3868cf57cd95d3d4fef9582e8
Hanai <yuuukariforev626111@gmail.com>
2026-07-18 16:19:26 +0900

    構造体ポインタについて補足

--- section_5_pointer/lesson_36/lesson36.go
+++ section_5_pointer/lesson_36/lesson36.go
@@ -11,25 +11,44 @@ type Vertex struct {
 	S string
 }
 
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000
+}
+
 func main() {
-	v1 := Vertex{X: 1, Y: 2, S: "test"} // 構造体メンバを指定して代入
-	fmt.Println(v1)
+	v := Vertex{1, 2, "test"}
+	changeVertex(v)
+	fmt.Println(v)
 
-	v2 := Vertex{} // メンバに値を渡さない -> メンバの方のデフォルト値が入る
+	v2 := &Vertex{1, 2, "test"}
+	changeVertex2(v2)
 	fmt.Println(v2)
+	fmt.Println(*v2)
 
-	v3 := Vertex{1, 4, "Test"} // member指定して値を渡さない
-	fmt.Println(v3)
+	/*
+		v1 := Vertex{X: 1, Y: 2, S: "test"} // 構造体メンバを指定して代入
+		fmt.Println(v1)
 
-	var v4 Vertex // typedef みたいな記載の仕方
-	fmt.Println(v4)
-	v4.X = 1
-	fmt.Println(v4)
+		v2 := Vertex{} // メンバに値を渡さない -> メンバの方のデフォルト値が入る
+		fmt.Println(v2)
 
-	// 構造体pointer
-	v5 := new(Vertex)
-	fmt.Println(v5)
+		v3 := Vertex{1, 4, "Test"} // member指定して値を渡さない
+		fmt.Println(v3)
 
-	v6 := &Vertex{}
-	fmt.Println(v6)
+		var v4 Vertex // typedef みたいな記載の仕方
+		fmt.Println(v4)
+		v4.X = 1
+		fmt.Println(v4)
+
+		// 構造体pointer
+		v5 := new(Vertex)
+		fmt.Println(v5)
+
+		v6 := &Vertex{}
+		fmt.Println(v6)
+	*/
 }
