acb36244993d92f606dd4366f3d5527a1873477e
Hanai <yuuukariforev626111@gmail.com>
2026-07-18 15:42:59 +0900

    nil pointer について記載

--- section_5_pointer/lesson_35/lesson35.go
+++ section_5_pointer/lesson_35/lesson35.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func nil_pointer() {
+	// 通常
+	var n *int
+	fmt.Println(n)
+	// ここでインクリメントする -> nil pointer に対して値を追加できない
+	*n++
+	fmt.Println(n)
+}
+
 // new , make の違いについて
 func main() {
 	// 値を格納しないがメモリの領域を確保したい
@@ -13,4 +22,6 @@ func main() {
 	// メモリを確保する記載の仕方(new を記載する)
 	var n_2 *int = new(int)
 	fmt.Println(n_2)
+
+	nil_pointer()
 }
