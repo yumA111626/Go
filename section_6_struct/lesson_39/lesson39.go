2e697392564cafde0473e59b4461189a0a1221f1
Hanai <yuuukariforev626111@gmail.com>
2026-07-20 16:23:48 +0900

    ポインタレシーバーについて記載

--- section_6_struct/lesson_39/lesson39.go
+++ section_6_struct/lesson_39/lesson39.go
@@ -11,6 +11,12 @@ func area(v Vertex) int {
 	return v.X * v.Y
 }
 
+// ポインタレシーバ
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
 // 擬似メソッドの書き方
 func (v Vertex) Area() int {
 	return v.X * v.Y
@@ -19,6 +25,8 @@ func (v Vertex) Area() int {
 // メソッドについての講義
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(area(v))
+	// fmt.Println(area(v))
+	fmt.Println(v.Area())
+	v.Scale(10) // 値の書き換え
 	fmt.Println(v.Area())
 }
