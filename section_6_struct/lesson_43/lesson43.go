9117098224457944bb332be465bb8cdf9bf8ccc4
Hanai <yuuukariforev626111@gmail.com>
2026-07-21 16:11:16 +0900

    インターフェース・ダックタイピングに関して記載

--- section_6_struct/lesson_43/lesson43.go
+++ section_6_struct/lesson_43/lesson43.go
@@ -3,19 +3,30 @@ package main
 import "fmt"
 
 type Human interface {
-	Say()
+	Say() string
 }
 
 type Person struct {
 	Name string
 }
 
-func (p Person) Say() {
-	fmt.Println(p.Name)
+// 値	を更新処理の場合の記載の仕方
+func (p *Person) Say() string {
+	p.Name = "Mr." + p.Name
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr.Mike" {
+		fmt.Println("Run")
+	} else {
+		fmt.Println("Get out")
+	}
 }
 
 // インターフェースに関しての講義
 func main() {
-	var Mike Human = Person{"Mike"}
-	Mike.Say()
+	var Mike Human = &Person{"Mike"}
+	// Mike.Say()
+	DriveCar(Mike)
 }
